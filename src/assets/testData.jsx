33dfe7eac0e2a23da1b227d4eb722113aebe8823
Niels Thuis <cnops_niels@hotmail.com>
2023-03-13 23:06:40 +0100

    Update test data to new DB version

--- src/assets/testData.jsx
+++ src/assets/testData.jsx
@@ -1,26 +1,56 @@
 const data = {
+  type: {
+    id: 1,
+    typeName: "Shopping",
+  },
+
   category: {
     id: 1,
-    name: "test",
-    lists: [
-      {
-        id: 1,
-        name: "listName 1",
-        headColor: "red",
-        listItems: [
-          { id: 1, name: "gras afrijden", completed: false },
-          { id: 2, name: "terras kuisen", completed: true },
-        ],
-      },
-      {
-        id: 2,
-        name: "listName 2",
-        headColor: "green",
-        listItems: [
-          { id: 3, name: "Banen kopen", completed: false },
-          { id: 4, name: "appels kopen", completed: true },
-        ],
-      },
-    ],
+    categoryName: "Full Stack Developer",
   },
+
+  lists: [
+    {
+      id: 1,
+      type: 1,
+      name: "listName 1",
+      headColor: "red",
+      listItems: [
+        { id: 1, name: "gras afrijden", completed: false },
+        { id: 2, name: "terras kuisen", completed: true },
+      ],
+    },
+    {
+      id: 2,
+      type: 1,
+      name: "listName 2",
+      headColor: "green",
+      listItems: [
+        { id: 3, name: "Banen kopen", completed: false },
+        { id: 4, name: "appels kopen", completed: true },
+      ],
+    },
+    {
+      id: 3,
+      type: 1,
+      category: 1,
+      name: "listName 3",
+      headColor: "purple",
+      listItems: [
+        { id: 5, name: "gras afrijden", completed: false },
+        { id: 6, name: "terras kuisen", completed: true },
+      ],
+    },
+    {
+      id: 4,
+      type: 1,
+      category: 1,
+      name: "listName 4",
+      headColor: "orange",
+      listItems: [
+        { id: 7, name: "Banen kopen", completed: false },
+        { id: 8, name: "appels kopen", completed: true },
+      ],
+    },
+  ],
 };
