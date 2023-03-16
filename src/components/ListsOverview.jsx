3652b2e099a1395738a476be8ebf2f777b154344
Niels Thuis <cnops_niels@hotmail.com>
2023-03-16 01:01:21 +0100

    removed some unneeded code

--- src/components/ListsOverview.jsx
+++ src/components/ListsOverview.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import config from "../config";
-import "../scss/listsOverview.scss";
+import "bulma/css/bulma.css";
 
 const ListsOverview = (props) => {
   // const [lists, setListsData] = useState([]);
