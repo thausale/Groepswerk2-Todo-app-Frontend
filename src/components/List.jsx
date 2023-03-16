fba3983ed8639c08fd039dacc0403db8a4c67738
Niels Thuis <cnops_niels@hotmail.com>
2023-03-16 20:48:29 +0100

    Added theoratical fetch function

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import config from "../config";
 
 const List = () => {
   const { id } = useParams();
   const [listName, setListName] = useState("");
   const [listItems, setListItems] = useState([]);
+  const baseUrl = config.apiBaseUrl;
+
+  const getAllByListId = async (id) => {
+    const { data } = await axios(baseUrl + "/list/" + id);
+    return data;
+  };
+
+  //"SELECT list_item.id, list_item.name as list_item_name, checked, list.name AS list_name, list.id as list_id
+  // FROM list_item
+  // INNER JOIN list ON list_item.list_id = list.id;
+  // WHERE list.id = " + id
+
   useEffect(() => {
     (async function () {
       const data = await getAllByListId(id);
