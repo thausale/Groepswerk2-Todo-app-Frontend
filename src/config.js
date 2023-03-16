293f0873470b025a58a6b06f58d97e6374e0f2b5
Niels Cnops <cnops_niels@hotmail.com>
2023-03-16 10:17:00 +0100

    reviewed
    changed production url
    Moved Bulma from ListsOverview to main.jsx

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const apiBaseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://s6.syntradeveloper.be/todo"
+    ? "https://s6.syntradeveloper.be/todo/api"
     : "http://localhost/api";
 
 export default {
