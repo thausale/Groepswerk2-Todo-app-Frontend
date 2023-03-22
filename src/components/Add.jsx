a6f83ddf439ff13759a29eb5a9a74d7624db91b5
Rinzin Tenzin <tenzin.rire@gmail.com>
2023-03-23 00:44:19 +0100

    8. add new list item functionality

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 const Add = ({ placeholder, postValue, setPostValue, baseUrl }) => {
   const [input, setInput] = useState("");
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -18,21 +18,40 @@ const List = () => {
 
   const [postValue, setPostValue] = useState("");
 
+  const fetchData = async () => {
+    try {
+      const {
+        data: { data },
+      } = await axios(`${baseUrl}/list/${id}`);
+      setList(data);
+      setListItems(data.listItems);
+      // console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addTodo = async () => {
+    setInputError(false);
+    if (postValue.length < 1) {
+      setInputError("list item can't be empty");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("name", postValue);
+    formData.append("list_id", id);
+    await axios.post(baseUrl + "/todo", formData);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const {
-          data: { data },
-        } = await axios(`${baseUrl}/list/${id}`);
-        setList(data);
-        setListItems(data.listItems);
-        // console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchData();
   }, [handledCheck]);
+
+  useEffect(() => {
+    addTodo();
+    fetchData();
+  }, [postValue]);
 
   useEffect(() => {
     setCheckedItems(listItems.filter((item) => item.checked == "1"));
@@ -47,6 +66,7 @@ const List = () => {
         placeholder="Add To Do"
         baseUrl={baseUrl}
         postValue={postValue}
+        setPostValue={setPostValue}
         inputError={inputError}
         back
         settings
