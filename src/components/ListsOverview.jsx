cce26c9f344827f28af286dabb7d99cb5b593d03
Rinzin Tenzin <tenzin.rire@gmail.com>
2023-03-23 20:29:27 +0100

    92. deleting all shop related code

--- src/components/ListsOverview.jsx
+++ src/components/ListsOverview.jsx
@@ -13,11 +13,8 @@ const ListsOverview = (props) => {
   const [allCats, setAllCats] = useState([]);
   const [impLists, setImpLists] = useState([]);
   const [lists, setLists] = useState([]);
-  const [shopping, setShopping] = useState([]);
   const [listsWCat, setListsWCat] = useState([]);
-  const [shoppingWCat, setShoppingWCat] = useState([]);
   const [listCats, setListCats] = useState([]);
-  const [shopCats, setShopCats] = useState([]);
   const [inputError, setInputError] = useState(false);
 
   const [postValue, setPostValue] = useState("");
@@ -64,10 +61,8 @@ const ListsOverview = (props) => {
   useEffect(() => {
     addNewList();
     const fetchLists = async () => {
-      const allLists = await getLists();
-      setAllLists(allLists);
-      const allCats = await getCats();
-      setAllCats(allLists);
+      setAllLists(await getLists());
+      setAllCats(await getCats());
 
       // get all lists with imp = 1;
       const importantLists = allLists.filter((list) => list.important === "1");
@@ -76,27 +71,16 @@ const ListsOverview = (props) => {
       const listsType1 = allLists.filter(
         (list) => list.type_name === "lists" && list.important === "0"
       );
-      const shoppingType2 = allLists.filter(
-        (list) => list.type_name === "shopping" && list.important === "0"
-      );
 
       const listsWoCat = listsType1.filter(
         (list) => list.category_name === null
       );
       setLists(listsWoCat);
-      const shoppingWoCat = shoppingType2.filter(
-        (list) => list.category_name === null
-      );
-      setShopping(shoppingWoCat);
 
       const listsWCat = listsType1.filter(
         (list) => list.category_name !== null
       );
       setListsWCat(listsWCat);
-      const shoppingWCat = shoppingType2.filter(
-        (list) => list.category_name !== null
-      );
-      setShoppingWCat(shoppingWCat);
 
       const listCategoriesOccurence = listsType1.reduce((listObj, el) => {
         if (el.category_name !== null) {
@@ -104,26 +88,11 @@ const ListsOverview = (props) => {
         }
         return listObj;
       }, {});
-      const shoppingCategoriesOccurence = shoppingType2.reduce(
-        (listObj, el) => {
-          if (el.category_name !== null) {
-            listObj[el.category_name] = listObj[el.category_name] + 1 || 1;
-          }
-          return listObj;
-        },
-        {}
-      );
 
       const listCats = allCats.filter((cat) =>
         Object.keys(listCategoriesOccurence).includes(cat.name)
       );
       setListCats(listCats);
-      // console.log(listCats);
-      const shopCats = allCats.filter((cat) =>
-        Object.keys(shoppingCategoriesOccurence).includes(cat.name)
-      );
-      setShopCats(shopCats);
-      // console.log(shopCats);
     };
     fetchLists();
   }, [postValue]);
@@ -143,9 +112,6 @@ const ListsOverview = (props) => {
       <MenuSection lists={lists} labelName="Lists">
         <Category cats={listCats} listsWCat={listsWCat}></Category>
       </MenuSection>
-      {/* <MenuSection lists={lists} labelName="Shopping">
-        <Category cats={shopCats} listsWCat={shoppingWCat}></Category>
-      </MenuSection> */}
     </Section>
   );
 };
